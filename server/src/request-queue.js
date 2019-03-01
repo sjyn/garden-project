9c461a344ad7dd4f0a0dfbcc3eb4e023bd70bf66
Steven <srosendahl@circadnece.com>
2019-02-28 18:30:42 -0700

    minor updates

--- server/src/request-queue.js
+++ server/src/request-queue.js
@@ -14,7 +14,6 @@ class RequestStack {
 
   popRequestForBoard(boardId) {
     const requestQueue = this.requestHolder[boardId];
-    console.log('requestQueue', requestQueue)
     if (!!requestQueue) {
       return requestQueue.pop();
     }
